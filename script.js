45eeff64d85ed1dd313ad7c8794cbf9ac32ed255
Darcocraft <69122588+Darcocraft@users.noreply.github.com>
2022-07-15 22:26:59 -0400

    First commit

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 function convertToRoman(num) {
-  const symboles = ["I", 'V', 'X', 'L', 'C', 'D', 'M']
-  let numerals = ''
-  function getNumerals(digit, scl) {
-    const i = symboles[scl * 2]
-    const v = symboles[scl * 2 + 1]
-    const a = parseInt(digit)
-    if (a <= 3) return i.repeat(a)
-    switch (a) {
+  const symboles = ["I", "V", "X", "L", "C", "D", "M"];
+  let numerals = "";
+
+  //converts single digit with place value to roman numerals
+  function getNumerals(digit, place) {
+    const i = symboles[place * 2];
+    const v = symboles[place * 2 + 1];
+    const x = symboles[place * 2 + 2];
+
+    digit = parseInt(digit);
+    if (digit <= 3) return i.repeat(digit);
+    switch (digit) {
       case 4:
-        return i + v
+        return i + v;
       case 5:
-        return v
+        return v;
       case 6:
-        return v + i
+        return v + i;
       case 7:
-        return v + i + i
+        return v + i + i;
       case 8:
-        return v + i + i + i
+        return v + i + i + i;
       case 9:
-        return i + symboles[scl * 2 + 2]
+        return i + x;
     }
   }
-  const str = num.toString().split('').reverse()
+
+  const str = num.toString().split("").reverse();
   for (let i = 0; i < str.length; i++) {
-    console.log(getNumerals(str[i], i))
-    numerals = getNumerals(str[i], i) + numerals
+    numerals = getNumerals(str[i], i) + numerals;
   }
-  console.log(num, numerals)
   return numerals;
 }
-
-convertToRoman(31);
-
-// Git message test
